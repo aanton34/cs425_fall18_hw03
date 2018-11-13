1e515cb764166eb74a425c6ad3b5ac065920af04
aanton34 <31954744+aanton34@users.noreply.github.com>
2018-11-13 15:27:39 +0200

    Initial Commit
    -> Favicon to be used by the pages
    -> XML format for my questions
    -> Initial Format of index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CS425 Quiz Game</title>
+    <meta name="keywords" content="HW3, CS425, Fall2018, PHP, Home Page">
+    <meta name="author" content="Antonia Antoniou">
+    <meta name="description" content="Homework 3 for CS425 Fall2018, University of Cyprus - Home Page">
+    <link rel="shortcut icon" href="favicon/question_block"/>
+</head>
+<body>
+    
+</body>
+</html>
