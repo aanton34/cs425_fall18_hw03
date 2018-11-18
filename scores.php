9a36f3425e31883abc9c3b0f1fbad439fd0d5056
aanton34 <31954744+aanton34@users.noreply.github.com>
2018-11-18 12:20:41 +0200

    Added High Scores into scores.php

--- scores.php
+++ scores.php
@@ -18,15 +18,64 @@
     </script>
 </head>
 <body>
-    <nav id="navbar">
+   <nav id="navbar">
         <ul>
             <li><a href="index.php">Home Page</a></li>
             <li><a href="help.php">Help</a></li>
             <li><a href="scores.php">Scores Board</a></li>
         </ul>
-    </nav>      
+    </nav>
+	<div class="container">
+		<h3 id="highScores">High Scores - Top 20</h3>
+		<?php 
+			$i = 0;
+			if($fp = fopen('scores.txt','r')){
+				while(!feof($fp)){
+					$line = fgets($fp);
+					$lineArray = split (",", $line);
+					$nicknameTable[$i] = $lineArray[0];
+					$temp = split("\n", $lineArray[1]);
+					$scoreTable[$i] = (int)$temp[0];
+					$i++;
+				}
+				fclose($fp);
+			}
+			array_multisort($scoreTable,SORT_DESC,$nicknameTable);
+		?>
+		<table id="scoreList">
+			<tr>
+				<th>#</th>
+				<th>Nickname</th>
+				<th>Score</th>
+			</tr>
+			<?php
+				if(sizeof($scoreTable) <= 20){
+					for($i=1;$i<=sizeof($scoreTable);$i++){
+			?>
+			<tr>
+				<th><?php echo $i?></th>
+				<th><?php echo $nicknameTable[$i-1]?></th>
+				<th><?php echo $scoreTable[$i-1]?></th>
+			</tr>
+			<?php
+					}
+				}
+				else{
+					for($i=1;$i<=20;$i++){
+			?>
+			<tr>
+				<th><?php echo $i?></th>
+				<th><?php echo $nicknameTable[$i-1]?></th>
+				<th><?php echo $scoreTable[$i-1]?></th>
+			</tr>
+			<?php
+					}
+				}
+			?>
+		</table>
+	</div>    
     <footer>
-	<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+		<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
     </footer>
 </body>
 </html>
